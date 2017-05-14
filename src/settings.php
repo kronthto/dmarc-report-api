08e7289439ddf5eddc6f5798d02282e24c615a85
Tobias Kronthaler <kronthto@gmail.com>
2017-05-14 18:42:55 +0200

    Load settings from env

--- src/settings.php
+++ src/settings.php
@@ -1,8 +1,23 @@
 <?php
 
+use Cekurte\Environment\Environment as Env;
+
+$appBase = __DIR__.'/..';
+
+$dotenv = new Dotenv\Dotenv($appBase);
+$dotenv->load();
+
 return [
     'settings' => [
-        'displayErrorDetails' => true, // set to false in production
+        'displayErrorDetails' => Env::get('APP_ENV') != 'production',
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+        'tmpReports' => $appBase.'/storage/reports/',
+
+        'db' => [
+            'host' => Env::get('DB_HOST', '127.0.0.1'),
+            'user' => Env::get('DB_USERNAME', 'homestead'),
+            'pass' => Env::get('DB_PASSWORD', 'secret'),
+            'db' => Env::get('DB_DATABASE', 'homestead'),
+        ],
     ],
 ];
