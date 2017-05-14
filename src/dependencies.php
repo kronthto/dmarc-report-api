f2aeb88229d4f6935405475a8d7f1f998ad28d6a
Tobias Kronthaler <kronthto@gmail.com>
2017-05-14 18:43:25 +0200

    Added DMARC-parser to Container

--- src/dependencies.php
+++ src/dependencies.php
@@ -2,4 +2,15 @@
 
 // DIC configuration
 
+use Psr\Container\ContainerInterface;
+
 $container = $app->getContainer();
+
+$container['dmarcparser'] = function (ContainerInterface $c) {
+    $dbSettings = $c->get('settings')['db'];
+
+    $parser = new Solaris\DmarcAggregateParser($dbSettings['host'], $dbSettings['user'], $dbSettings['pass'],
+        $dbSettings['db']);
+
+    return $parser;
+};
